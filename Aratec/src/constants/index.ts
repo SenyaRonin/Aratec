f40ce7046350f147bdc4caa5c1584faeb86a3ea8
SenyaRonin <109452832+SenyaRonin@users.noreply.github.com>
2024-03-01 17:52:06 +0400

    Aratec_Project_Prototype

---
+++ Aratec/src/constants/index.ts
@@ -0,0 +1,65 @@
+export const sidebarLinks = [
+  {
+    imgURL: "/assets/icons/home.png",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/assets/icons/files.png",
+    route: "/explore",
+    label: "Files",
+  },
+  {
+    imgURL: "/assets/icons/contacts.png",
+    route: "/all-users",
+    label: "Contacts",
+  },
+  {
+    imgURL: "/assets/icons/saved.png",
+    route: "/saved",
+    label: "Saved",
+  },
+  {
+    imgURL: "/assets/icons/upload.png",
+    route: "/create-post",
+    label: "Upload file",
+  },
+  {
+    imgURL: "/assets/icons/subscribe.png",
+    route: "/subscribe",
+    label: "Subscribe",
+  },
+];
+
+export const bottombarLinks = [
+  {
+    imgURL: "/assets/icons/home.png",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/assets/icons/files.png",
+    route: "/explore",
+    label: "Files",
+  },
+  {
+    imgURL: "/assets/icons/contacts.png",
+    route: "/all-users",
+    label: "Contacts",
+  },
+  {
+    imgURL: "/assets/icons/saved.png",
+    route: "/saved",
+    label: "Saved",
+  },
+  {
+    imgURL: "/assets/icons/upload.png",
+    route: "/create-post",
+    label: "Upload",
+  },
+  {
+    imgURL: "/assets/icons/subscribe.png",
+    route: "/subscribe",
+    label: "Subscribe",
+  },
+];
